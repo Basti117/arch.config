929bc71c6fcb56f46f607416d63b73e58bdc4f15
Basti117 <sebastian.schneider117@gmail.com>
2026-02-08 18:01:20 +0100

    fix: only run hyprland monitor configruation script if there are two monitors

--- arch/config/roles/hyprland/files/.config/hypr/configure-monitor-layout.sh
+++ arch/config/roles/hyprland/files/.config/hypr/configure-monitor-layout.sh
@@ -2,7 +2,10 @@
 
 monitors=$(hyprctl monitors -j)
 
-# only run this if there a two monitors
+# only run if exactly two monitors
+count=$(echo "$monitors" | jq "length")
+[[ "$count" -eq 2 ]] || exit 0
+
 right=$(echo "${monitors}" | jq -r "sort_by(.id) | .[0].name")
 left=$(echo "${monitors}" | jq -r "sort_by(.id) | .[1].name")
 
