87357b5ff2dfe11c6a86f64bbfac5df36d07d845
Basti117 <sebastian.schneider117@gmail.com>
2026-03-01 10:25:19 +0100

    feat: support multiple wallpapers

--- roles/dotfiles/files/.config/hueshift/scripts/reload_starship.sh
+++ roles/dotfiles/files/.config/hueshift/scripts/reload_starship.sh
@@ -1,6 +1,17 @@
 for socket in /tmp/kitty-commands*; do
     if [ -S "$socket" ]; then
-	echo "Sending stuff to $socket"
-        kitty @ --to "unix:$socket" send-text --match 'all' 'clear\r'
+        # Korrigierter jq-Pfad: .[].tabs[].windows[]
+        # Wir greifen auf das erste Element des Arrays foreground_processes zu: .[0]
+        ids=$(kitty @ --to "unix:$socket" ls | jq -r '
+            .[] | .tabs[].windows[] 
+
+            | select(.foreground_processes[0].cmdline[0] | test("sh|bash|zsh|fish")) 
+            | select(.foreground_processes | length == 1) 
+            | .id' 2>/dev/null)
+
+        for id in $ids; do
+            echo "Clearing idle window $id on $socket"
+            kitty @ --to "unix:$socket" send-text --match "id:$id" 'clear\r'
+        done
     fi
 done
