bd628679756ac6d57e464d1f1e20858f4e9ed9e3
Basti117 <sebastian.schneider117@gmail.com>
2026-03-08 17:11:48 +0100

    refactor: rename hueshift to cub + rename role hyprland to desktop + remove hyprpaper

--- roles/dotfiles/files/.config/cub/scripts/reload_starship.sh
+++ roles/dotfiles/files/.config/cub/scripts/reload_starship.sh
@@ -1,17 +1,16 @@
+CURRENT_ID=${KITTY_WINDOW_ID:-0}
 for socket in /tmp/kitty-commands*; do
     if [ -S "$socket" ]; then
-        # Korrigierter jq-Pfad: .[].tabs[].windows[]
-        # Wir greifen auf das erste Element des Arrays foreground_processes zu: .[0]
-        ids=$(kitty @ --to "unix:$socket" ls | jq -r '
+        ids=$(kitty @ --to "unix:$socket" ls | jq -r --arg current_id "$CURRENT_ID" '
             .[] | .tabs[].windows[] 
-
             | select(.foreground_processes[0].cmdline[0] | test("sh|bash|zsh|fish")) 
-            | select(.foreground_processes | length == 1) 
+            | select(.foreground_processes | length == 1)
+	    | select(.id != $current_id)
             | .id' 2>/dev/null)
 
         for id in $ids; do
             echo "Clearing idle window $id on $socket"
-            kitty @ --to "unix:$socket" send-text --match "id:$id" 'clear\r'
+            kitty @ --to "unix:$socket" send-text --match "id:$id" "clear\r"
         done
     fi
 done
